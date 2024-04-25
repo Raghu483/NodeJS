2e3b7ace8dd6b0eec8cb4ec1881960242e201aa2
Raghu Teja <raghu.teja483@gmail.com>
2024-04-26 00:31:59 +0530

    intial commit

---
+++ task-manager/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const tasks = require('./task.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Something bad happened', err);
+    }
+    console.log(`Server is listening on ${port}`);
+});
+
+app.get("/",(req,res) =>{
+    console.log("Hello World");
+    res.send("Server is up");
+});
+
+app.get("/tasks", (req,res)=>{
+   res.type("application/json").status(200).send(tasks);
+});
+
+module.exports = app;
