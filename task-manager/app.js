f41cee5bfadecfecb9e646a256eeab8e9adca8e1
Raghu Teja <raghu.teja483@gmail.com>
2024-04-27 12:38:01 +0530

    few changes

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const tasks = require('./task.json');
+var tasksConfig = require('./task.json');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +21,38 @@ app.get("/",(req,res) =>{
 app.get("/tasks", (req,res)=>{
    res.type("application/json").status(200).send(tasks);
 });
-//test
+
+app.get("/tasks/:id", (req,res) =>{
+    const tasks =  tasksConfig.tasks;
+    let taskIds  = tasks.filter(task => task.id === parseInt(req.params.id));
+    if(taskIds.length == 0){
+        return res.status(404).send("task is not avalible");
+    }
+    let jsonString = JSON.stringify(taskIds);
+    res.status(200).send(jsonString);
+});
+
+app.post("/tasks",(req,res) => {
+    const tasks =  tasksConfig.tasks;
+    console.log("body is",req.body);
+    let parsedBody =  JSON.parse(req.body);
+    tasks.push(req.body);
+    //console.log("tasks",tasks);
+
+    res.status(200).send("Task Created Sucessfully");
+});
+
+app.put("/tasks/:id", (req,res) =>{
+    const tasks =  tasksConfig.tasks;
+    //console.log("body is",req.body);
+    //console.log(tasks.length);
+    let taskIds  = tasks.filter(task => task.id === parseInt(req.params.id));
+    if(taskIds.length == 0){
+        return res.status(404).send("task is not avalible");
+    }
+    let jsonString = JSON.stringify(taskIds);
+    res.status(200).send(jsonString);
+});
+
 
 module.exports = app;
