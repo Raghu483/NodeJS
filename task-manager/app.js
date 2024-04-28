74ca9b8640d3d7797c2a313288101d6a64f09442
Raghu Teja <raghu.teja483@gmail.com>
2024-04-29 03:14:44 +0530

    Added all the resource

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var tasksConfig = require('./task.json');
+var tasksConfig = require('./task.json').tasks;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,13 +19,13 @@ app.get("/",(req,res) =>{
 });
 
 app.get("/tasks", (req,res)=>{
-   res.type("application/json").status(200).send(tasks);
+   res.type("application/json").status(200).send(tasksConfig);
 });
 
 app.get("/tasks/:id", (req,res) =>{
-    const tasks =  tasksConfig.tasks;
+    const tasks =  tasksConfig;
     let taskIds  = tasks.filter(task => task.id === parseInt(req.params.id));
-    if(taskIds.length == 0){
+    if(!taskIds ||taskIds.length == 0){
         return res.status(404).send("task is not avalible");
     }
     let jsonString = JSON.stringify(taskIds);
@@ -33,25 +33,50 @@ app.get("/tasks/:id", (req,res) =>{
 });
 
 app.post("/tasks",(req,res) => {
-    const tasks =  tasksConfig.tasks;
-    console.log("body is",req.body);
-    let parsedBody =  JSON.parse(req.body);
-    tasks.push(req.body);
-    //console.log("tasks",tasks);
-
-    res.status(200).send("Task Created Sucessfully");
+    const tasks =  tasksConfig;
+    const parsedBody =  req.body;
+    const { title, description,completed} = req.body;
+    if(title && description && typeof completed !== 'undefined'){
+       parsedBody.id = tasks.length+1;
+       tasks.push(parsedBody);
+      return res.status(200).send("Task Created Sucessfully");
+    }else{
+        return res.status(400).send("Plese fill mandatory fields");
+    }
 });
 
 app.put("/tasks/:id", (req,res) =>{
-    const tasks =  tasksConfig.tasks;
-    //console.log("body is",req.body);
-    //console.log(tasks.length);
-    let taskIds  = tasks.filter(task => task.id === parseInt(req.params.id));
-    if(taskIds.length == 0){
+    const tasks =  tasksConfig;
+    const taskIds  = tasks.find(task => task.id === parseInt(req.params.id));
+    console.log("tasks",taskIds);
+    if(!taskIds || taskIds.length == 0){
         return res.status(404).send("task is not avalible");
     }
-    let jsonString = JSON.stringify(taskIds);
-    res.status(200).send(jsonString);
+    const { title, description,completed} = req.body;
+    if(title && description && typeof completed !== 'undefined'){
+        taskIds.title = req.body.title;
+        taskIds.description = req.body.description;
+        taskIds.completed = req.body.completed;
+        console.log("updated task",taskIds);
+        res.status(200).send("Task Updated Sucessfully");
+    }else{
+        return res.status(400).send("Plese fill mandatory fields");
+    }
+    
+});
+
+app.delete("/tasks/:id", (req,res) =>{
+    const taskIds  = tasksConfig.find(task => task.id === parseInt(req.params.id));
+    console.log(taskIds);
+    if(!taskIds || taskIds.length<=0){
+        return res.status(404).send("task is not avalible");
+    }
+    console.log("taskIds",taskIds);
+    const index = tasksConfig.indexOf(taskIds);
+    console.log("index",index);
+    tasksConfig.splice(index,1);
+    console.log("taskConfgi",tasksConfig);
+    res.status(200).send("Task Deleted Sucessfully");
 });
 
 
